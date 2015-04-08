fc1da239fe376a597ff30d43af9482bf5af1901e
armi1189 <armi1189@gmail.com>
2015-04-08 11:19:53 +0100

    DB configuration

---
+++ spec/link_spec.rb
@@ -0,0 +1,17 @@
+describe Link do
+
+  context 'Demostration of how datamapper works' do
+    it 'is created and then retrieved from the db' do
+      expect(described_class.count).to eq(0)
+      described_class.create(title: 'Makers Academy',
+                             url: 'http://www.makersacademy.com/')
+      expect(described_class.count).to eq(1)
+      link = described_class.first
+      expect(link.url).to eq('http://www.makersacademy.com/')
+      expect(link.title).to eq('Makers Academy')
+      link.destroy
+      expect(described_class.count).to eq(0)
+    end
+  end
+
+end
